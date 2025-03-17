7a63eec77ea28697223cccda190f4cafa49a95d4
bot_fx <oluwadamilareobalana@gmail.com>
2025-03-17 11:51:03 +0100

    Add customizable alert to index.html

---
+++ src/js/alert.js
@@ -0,0 +1,33 @@
+export default class Alert {
+    constructor() {
+        this.url = './json/alerts.json';
+    }
+    async init() {
+        const data = await this.fetchData();
+        createData(data);
+        console.log(data);
+    }
+    async fetchData(){
+        const data = await fetch(this.url);
+        if(data.ok){
+            const result = data.json();
+            return result;
+        }
+    }
+}
+
+function createData(data){
+    data.forEach(datum => {
+        const section = document.createElement("section");
+        section.classList.add("alert-list");
+        section.style.backgroundColor = datum.background;
+        section.style.color = datum.color;
+
+        const text = document.createElement("p");
+        text.textContent = datum.message;
+        text.style.textAlign = "center";
+
+        section.append(text);
+        document.querySelector("main").prepend(section);
+    });
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,11 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
+import Alert from "./alert";
 
 const dataSource = new ProductData("tents");
 const element = document.querySelector(".product-list");
 const listing = new ProductList("Tents", dataSource, element);
+const alert = new Alert();
 
 listing.init();
-document.addEventListener("DOMContentLoaded",()=>{
-    alert("Welcome to Sleep Outside");
-})
+alert.init();
