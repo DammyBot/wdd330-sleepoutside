b1f8c30d74fedbf8039bc58a4fcd193a36e49248
bot_fx <oluwadamilareobalana@gmail.com>
2025-03-17 12:03:35 +0100

    run build

--- src/js/alert.js
+++ src/js/alert.js
@@ -1,33 +1,32 @@
 export default class Alert {
-    constructor() {
-        this.url = './json/alerts.json';
+  constructor() {
+    this.url = "./json/alerts.json";
+  }
+  async init() {
+    const data = await this.fetchData();
+    createData(data);
+  }
+  async fetchData() {
+    const data = await fetch(this.url);
+    if (data.ok) {
+      const result = data.json();
+      return result;
     }
-    async init() {
-        const data = await this.fetchData();
-        createData(data);
-        console.log(data);
-    }
-    async fetchData(){
-        const data = await fetch(this.url);
-        if(data.ok){
-            const result = data.json();
-            return result;
-        }
-    }
+  }
 }
 
-function createData(data){
-    data.forEach(datum => {
-        const section = document.createElement("section");
-        section.classList.add("alert-list");
-        section.style.backgroundColor = datum.background;
-        section.style.color = datum.color;
+function createData(data) {
+  data.forEach((datum) => {
+    const section = document.createElement("section");
+    section.classList.add("alert-list");
+    section.style.backgroundColor = datum.background;
+    section.style.color = datum.color;
 
-        const text = document.createElement("p");
-        text.textContent = datum.message;
-        text.style.textAlign = "center";
+    const text = document.createElement("p");
+    text.textContent = datum.message;
+    text.style.textAlign = "center";
 
-        section.append(text);
-        document.querySelector("main").prepend(section);
-    });
+    section.append(text);
+    document.querySelector("main").prepend(section);
+  });
 }
